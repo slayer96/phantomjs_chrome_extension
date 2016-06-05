6a4a34161ea08241ba7a06aadc1ac23acba3da39
slayer96 <myhailo.mykytyn.96@gmail.com>
2016-06-06 01:09:03 +0300

    start working with script generator

---
+++ phantom.js
@@ -0,0 +1,18 @@
+
+
+if (typeof(EventTypes) == "undefined") {
+  EventTypes = {};
+}
+
+
+EventTypes.OpenURL = 0;
+EventTypes.Click = 2;
+EventTypes
+
+
+
+function PhantomRender(document) {
+	this.document = document;
+}
+
+
