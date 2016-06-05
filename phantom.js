0719d6d1a073f3756b9063f2734b4badad0937c7
slayer96 <myhailo.mykytyn.96@gmail.com>
2016-06-06 01:48:04 +0300

    working with script generator(1)

--- phantom.js
+++ phantom.js
@@ -1,5 +1,3 @@
-
-
 if (typeof(EventTypes) == "undefined") {
   EventTypes = {};
 }
@@ -7,12 +5,48 @@ if (typeof(EventTypes) == "undefined") {
 
 EventTypes.OpenURL = 0;
 EventTypes.Click = 2;
-
+EventTypes.PageLoad = 3;
 
 
 
 function PhantomRender(document) {
 	this.document = document;
+
 }
+
+PhantomRender.prototype.space = function() {
+	this.document.writeln('\n')
+}
+
+PhantomRender.prototype.text = function(_text) {
+	this.document.writeln(_text);
+}
+
+
+PhantomRender.prototype.render = function() {
+	this.writeHeader();
+}
+
+PhantomRender.prototype.writeHeader = function() {
+	this.text("/* ------------------------------- */", 0)
+	this.text("/* Phantom generator */", 0)
+	this.text("/* ------------------------------- */", 0)
+	this.space()
+}
+
+
+var script_generate = new PhantomRender(document);
+window.onload = function onpageload() {
+	var with_xy = false;
+  if(window.location.search=="?xy=true") {
+    with_xy = true;
+  }
+  chrome.runtime.sendMessage({}, function(response) {
+  	script_generate.render()
+  })
+}
+
+
+
 
 
