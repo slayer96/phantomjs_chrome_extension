6860709e3f458d6bb3f5740736730f16fb19cbfc
slayer96 <myhailo.mykytyn.96@gmail.com>
2016-06-06 13:06:50 +0300

    add button 'stop'

--- phantom.js
+++ phantom.js
@@ -1,5 +1,5 @@
 if (typeof(EventTypes) == "undefined") {
-  EventTypes = {};
+  	EventTypes = {};
 }
 
 
@@ -15,35 +15,43 @@ function PhantomRender(document) {
 }
 
 PhantomRender.prototype.space = function() {
-	this.document.writeln('\n')
+	this.document.write("\n");
 }
 
 PhantomRender.prototype.text = function(_text) {
 	this.document.writeln(_text);
 }
 
+PhantomRender.prototype.statement = function(_text, index) {
+  	if(indent==undefined) indent = 1;
+  	var output = (new Array(4*indent)).join(" ") + text;
+  	this.document.writeln(output);
+}
+}
 
 PhantomRender.prototype.render = function() {
 	this.writeHeader();
 }
 
 PhantomRender.prototype.writeHeader = function() {
-	this.text("/* ------------------------------- */", 0)
-	this.text("/* Phantom generator */", 0)
-	this.text("/* ------------------------------- */", 0)
-	this.space()
+	this.text("/* ------------------------------- */", 0);
+	this.text(" Phantom generator ", 0);
+	this.text(" --------------------------------- ", 0);
+	this.space();
+	this.statement("var page = require('webpage').create();", 0)
+	this.space();
 }
 
 
 var script_generate = new PhantomRender(document);
 window.onload = function onpageload() {
 	var with_xy = false;
-  if(window.location.search=="?xy=true") {
-    with_xy = true;
-  }
-  chrome.runtime.sendMessage({}, function(response) {
-  	script_generate.render()
-  })
+  	if(window.location.search=="?xy=true") {
+    	with_xy = true;
+  	}
+  	chrome.runtime.sendMessage({}, function(response) {
+  		script_generate.render()
+  	})
 }
 
 
